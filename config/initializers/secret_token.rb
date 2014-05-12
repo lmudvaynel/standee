b512f7ffe0ebc84549c3f0569cb2730493ed7c05
Demko <artdemko@gmail.com>
2014-05-12 13:24:44 +0300

    Deploy config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Standee::Application.config.secret_key_base = '8f62a5beb79b3dd7e0b7cedd6891e36a077188f7faec7e87e1509251c541bffc242e5122a8826bbc84c1e6ea33f3cf2a38c0932fae13117605528668e813240f'
