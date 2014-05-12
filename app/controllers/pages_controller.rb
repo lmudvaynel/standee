7e501c7f6c8aa0a98ff3d7c7f46db56870859a64
Demko <artdemko@gmail.com>
2014-05-12 14:06:54 +0300

    Add show action and validations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
+# encoding: utf-8
 class PagesController < ApplicationController
+  def show
+    @page = Page.friendly.find(params[:slug])
+    render params[:slug] if controller_view_exists?(params[:slug])
+  end
+
+  private
+
+  def view_exists?(view)
+    File.exists? Rails.root.join("app", "views", view)
+  end
+
+  def controller_view_exists?(name)
+    view_exists?("#{params[:controller]}/#{name}.html.erb")
+  end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,4 @@
 class Page < ActiveRecord::Base
+  validates :slug, presence: true, uniqueness: true
+  validates :name, presence: true
 end
